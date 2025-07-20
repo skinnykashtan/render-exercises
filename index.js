e9e053233784754837855cc683164da074ee2d57
Hubercik <veestereh@gmail.com>
2025-07-20 11:35:55 +0200

    fist commit

---
+++ index.js
@@ -0,0 +1,111 @@
+const express = require('express')
+const morgan = require('morgan')
+const {response} = require("express");
+const app = express()
+
+morgan.token('body', (req) => {
+    return JSON.stringify(req.body)
+})
+
+// app.use(express.static('dist'))
+
+app.use(express.json())
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+let date = new Date().toString()
+
+let persons = [
+    {
+      "id": "1",
+      "name": "Arto Hellas",
+      "number": "040-123456"
+    },
+    {
+      "id": "2",
+      "name": "Ada Lovelace",
+      "number": "39-44-5323523"
+    },
+    {
+      "id": "3",
+      "name": "Dan Abramov",
+      "number": "12-43-234345"
+    },
+    {
+      "id": "4",
+      "name": "Mary Poppendieck",
+      "number": "39-23-6423122"
+    }
+]
+
+const generateId = () => {
+    const maxId = persons.length > 0 ? Math.max(...persons.map(p => Number(p.id))) : 0
+    return String(maxId + 1)
+}
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+
+    if (!body.number || !body.name) {
+        return res.status(400).json({
+            error: 'number or name is missing'
+        })
+    }
+
+    const nameExists = persons.find(p => p.name === body.name)
+    if (nameExists) {
+        return res.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    }
+
+    persons = persons.concat(person)
+
+    res.json(person)
+
+})
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(p => p.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = req.params.id
+    persons = persons.filter(p => p.id !== id)
+
+    res.status(200).end()
+})
+
+app.get('/info', (request, response) => {
+   response.send(`<p>Phonebook has info for ${persons.reduce((acc) => acc + 1, 0)} people</p>
+    <p>${date}</p>`)
+})
+
+const unknownEndpoint = (req, res) => {
+    res.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const PORT = 3003
+app.listen(PORT)
+
+
+
