6b611e6af597bc1960169de18163327624f53859
Hubercik <veestereh@gmail.com>
2025-07-22 23:52:56 +0200

    usunieta tablica persons

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// require('dotenv').config()
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const {response, request} = require("express");
@@ -17,11 +17,6 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 
 let date = new Date().toString()
 
-const generateId = () => {
-    const maxId = persons.length > 0 ? Math.max(...persons.map(p => Number(p.id))) : 0
-    return String(maxId + 1)
-}
-
 app.post('/api/persons', (req, res) => {
     const body = req.body
 
@@ -31,25 +26,24 @@ app.post('/api/persons', (req, res) => {
         })
     }
 
-    const nameExists = persons.find(p => p.name === body.name)
-    if (nameExists) {
-        return res.status(400).json({
-            error: 'name must be unique'
-        })
-    }
-
-    const person = new Person({
-        name: body.name,
-        number: body.number
+    Person.exists({ name: body.name }).then((exists) => {
+        if (exists) {
+            return Promise.reject({ status: 400, body: {error: 'name must be unique'}});
+        }
+        return new Person({
+            name: body.name,
+            number: body.number
+        }).save();
+    }).then(savedPerson => {
+        res.status(201).json(savedPerson);
+    }).catch(err => {
+        res.status(400).json(err.message);
     })
 
-    person.save().then(savedPerson => {
-        res.json(savedPerson)
-    })
 })
 
 app.get('/', (req, res) => {
-  res.send('OK');
+    res.send('OK');
 });
 
 app.get('/api/persons', (request, response) => {
@@ -61,16 +55,16 @@ app.get('/api/persons', (request, response) => {
 app.get('/api/persons/:id', (request, response) => {
     Person.findById(request.params.id).then(person => {
         if (!person) {
-            return response.status(404).json({ error: 'Nie znaleziono uzytkownika' });
+            return response.status(404).json({error: 'Nie znaleziono uzytkownika'});
         }
         response.json(person)
     }).catch(error => {
         if (error.name === 'CastError') {
-            return response.send(400).json({error: 'Bledne id' });
+            return response.status(400).json({error: 'Bledne id'});
         }
 
         console.log(error)
-        response.status(500).json({ error: 'Cos poszlo nie tak...' });
+        response.status(500).json({error: 'Cos poszlo nie tak...'});
     })
 })
 
@@ -78,33 +72,33 @@ app.delete('/api/persons/:id', (req, res) => {
     Person.findByIdAndDelete(req.params.id)
         .then(deleted => {
             if (!deleted) {
-                return res.status(404).json({ error: 'Notatka nie znaleziona' });
+                return res.status(404).json({error: 'Notatka nie znaleziona'});
             }
             res.status(204).end();
         }).catch(error => {
         if (error.name === 'CastError') {
-            return res.send(400).json({error: 'Bledne id' });
+            return res.send(400).json({error: 'Bledne id'});
         }
 
         console.log(error)
-        res.status(500).json({ error: 'Cos poszlo nie tak...' });
+        res.status(500).json({error: 'Cos poszlo nie tak...'});
     })
 })
 
 app.get('/info', (request, response) => {
-   response.send(`<p>Phonebook has info for ${persons.reduce((acc) => acc + 1, 0)} people</p>
+    response.send(`<p>Phonebook has info for ${persons.reduce((acc) => acc + 1, 0)} people</p>
     <p>${date}</p>`)
 })
 
 const unknownEndpoint = (req, res) => {
-    res.status(404).send({ error: 'unknown endpoint' })
+    res.status(404).send({error: 'unknown endpoint'})
 }
 
 app.use(unknownEndpoint)
 
 const PORT = process.env.PORT || 3003;
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+    console.log(`Server listening on port ${PORT}`);
 });
 
 
